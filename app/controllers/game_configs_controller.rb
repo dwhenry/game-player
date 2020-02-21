5d1e1c0060180edc406e74658554c70825338a9c
David Henry <david@decoybecoy.com>
2020-02-21 09:41:39 +0000

    Swicth to PG

---
+++ app/controllers/game_configs_controller.rb
@@ -0,0 +1,19 @@
+class GameConfigsController < ApplicationController
+  def new
+    if params[:parent_id]
+      parent = GameConfig.find(params[:parent_id])
+      attrs = parent.attributes.except(:id, :created_at, :updated_at)
+      game_config = GameConfig.create!(attrs.merge(parent_id: parent.id))
+    else
+      game_config = GameConfig.create!(decks: { tasks: [], achievements: [], employees: [] })
+    end
+
+    redirect_to edit_game_config_path(game_config)
+  end
+
+  def edit
+    @game_config = GameConfig.find(params[:id])
+    path = params.path.split(',').map(&:to_i)
+    @item = @game_config.decks.dig(path)
+  end
+end

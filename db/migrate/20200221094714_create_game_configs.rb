809be65cc741aa89e6cc757c305b03ccbe957e66
David Henry <david@decoybecoy.com>
2020-02-21 10:01:27 +0000

    Add GameConfig class

---
+++ db/migrate/20200221094714_create_game_configs.rb
@@ -0,0 +1,14 @@
+class CreateGameConfigs < ActiveRecord::Migration[6.0]
+  def change
+    enable_extension 'pgcrypto'
+
+    create_table :game_configs, id: :uuid do |t|
+      t.jsonb :desks
+      t.uuid :parent_id
+      t.jsonb :rules
+      t.boolean :locked, default: false, null: false
+
+      t.timestamps
+    end
+  end
+end

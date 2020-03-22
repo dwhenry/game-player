e69502e1dac552205a1fe149ab26500fba626d3c
lamp <matthew.gradidge@gmail.com>
2020-03-22 19:08:48 -0400

    Potential nock mocing solution

--- test/javascript/createNewCard_test.js
+++ test/javascript/createNewCard_test.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { render, cleanup, waitForElement } from '@testing-library/react';
+import nock from 'nock';
+import { render, cleanup, waitForElement, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 // import 'jest-dom/extend-expect';
 import ConfigEditor from '../../app/javascript/components/ConfigEditor'
 
+
 describe('When editing an empty deck', () => {
   afterEach(cleanup);
 
-  it('allow a card to be added to the tasks deck', () => {
+
+  it('allow a card to be added to the tasks deck', async () => {
+    nock.recorder.rec()
+
+    const scope = nock("http://localhost:9876/")
+      .post('/game_configs/1')
+      .reply(200, {
+        license: {
+          key: 'mit',
+          name: 'MIT License',
+          spdx_id: 'MIT',
+          url: 'https://api.github.com/licenses/mit',
+          node_id: 'MDc6TGljZW5zZTEz',
+        },
+      });
+
     const { getByText, getByLabelText, getByPlaceholderText, queryByText } = render(
-      <ConfigEditor decks={{ tasks: [], achievements: [], employees: [] }} />
+      <ConfigEditor id={1} decks={{ tasks: [], achievements: [], employees: [] }} />
     );
     userEvent.type(getByLabelText('Name'), 'test card');
     userEvent.type(getByLabelText('Cost'), '1');
@@ -18,6 +35,10 @@ describe('When editing an empty deck', () => {
     userEvent.type(getByLabelText('Deck'), 'tasks');
     userEvent.type(getByLabelText('Number'), '3');
     userEvent.click(getByText('Save'));
+
+    let called = false
+    setTimeout(() => { called = true; console.log(nock.recorder.play()) }, 1000)
+    await waitFor(() => expect(called).toBe(true), {timeout: 2500})
 
     expect(1 + 1).toBe(2);
   })

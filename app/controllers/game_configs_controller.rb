9c6d5de93a92292ccf3749aa3b0aec8ab5cc6ac2
David Henry <david@decoybecoy.com>
2020-04-06 23:45:56 +0100

    Remove legacy update_all functionality (previous used for JSON editor)

--- app/controllers/game_configs_controller.rb
+++ app/controllers/game_configs_controller.rb
@@ -21,19 +21,6 @@ class GameConfigsController < ApplicationController
     @card = { id: '' }
   end
 
-  def update_all
-    @game_config = GameConfig.find(params[:id])
-    decks = params[:decks]
-    decks.each do |key, cards|
-      decks[key] = cards.each_with_object({}) { |card, hash| hash[card['id']] = card }
-    end
-    if @game_config.update(decks: decks)
-      render json: @game_config.as_json
-    else
-      render json: { error: "Update to update deck.. #{@game_config.errors.full_messages.join("\n")}" }, status: 500
-    end
-  end
-
 
   def update
     @game_config = GameConfig.find(params[:id])
